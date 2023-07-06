9c0a35f9e5bd8c7b9742b73899ad73c14ea356eb
Stefan Bohacek <stefan@stefanbohacek.com>
2023-07-06 13:27:48 -0400

    Handle invalid URLs.

--- modules/getStatusContext.js
+++ modules/getStatusContext.js
@@ -1,20 +1,26 @@
 const getStatusContext = async (statusURL) => {
   let results = [];
 
-  const url = new URL(statusURL)
-  const domain = url.hostname;
-  const postId = url.pathname.split('/')[2];
-  const apiURL = `https://${domain}/api/v1/statuses/${postId}/context`;
-  
   try {
-      const resp = await fetch(apiURL);
-      const respJSON = await resp.json();
-      results = respJSON;
-      return results;
+    const url = new URL(statusURL)
+    const domain = url.hostname;
+    const postId = url.pathname.split('/')[2];
+    const apiURL = `https://${domain}/api/v1/statuses/${postId}/context`;
+    
+    try {
+        const resp = await fetch(apiURL);
+        const respJSON = await resp.json();
+        results = respJSON;
+        return results;
+    } catch (error) {
+        console.log(error);
+        return results;
+    }
   } catch (error) {
-      console.log(error);
-      return results;
+    console.log(error);
+    return results;
   }
+
 }
 
 export default getStatusContext;
